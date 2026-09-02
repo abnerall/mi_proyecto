a0ca4beb3e8b1ffe3f9d2f1d66290dd8416b21f6
Helen Pacari <helenpacari2506@gmail.com>
2026-09-02 10:06:54 -0400

    Implementación de libreria canvas

--- app/js/main.js
+++ app/js/main.js
@@ -1,1 +1,11 @@
 document.getElementById('mensaje').textContent = '¡El servidor y el JavaScript están conectados con éxito!';
+import confetti from 'canvas-confetti';
+
+// Ejecutamos una animación de confetti cuando la página carga
+confetti({
+  particleCount: 100,
+  spread: 70,
+  origin: { y: 0.6 }
+});
+
+console.log('Librería canvas-confetti cargada correctamente.');
